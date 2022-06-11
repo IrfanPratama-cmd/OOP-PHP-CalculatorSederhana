fb6ff32e8c993170077b6adfec55eea039e10cba
Irfan Pratama <erfanpratama321@gmail.com>
2022-06-12 00:01:36 +0700

    update ui

--- kalkulator.php
+++ kalkulator.php
@@ -9,4 +9,26 @@ $bil2;
 $operasi;
 
 $calculator = new Calculator($bil1, $bil2, $operasi);
-echo $calculator->calcMethod();
+// echo $calculator->calcMethod();
+
+?>
+
+<html>
+
+<h2>Hasil</h2>
+
+<body>
+    <form>
+        <input type="text" name="bil1" value="<?= $bil1; ?>" readonly>
+        <input type="text" name="operasi" value="<?= $operasi; ?>" readonly>
+        <input type="text" name="bil2" value="<?= $bil2; ?>" readonly>
+        <p> = </p>
+        <input type="text" name="bil1" value="<?= $calculator->calcMethod(); ?>" readonly>
+    </form>
+
+
+
+    <a href="index.php">Kembali</a>
+</body>
+
+</html>
